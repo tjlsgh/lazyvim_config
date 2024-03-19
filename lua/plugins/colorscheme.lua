b03fa52a0df17c592d209c3506f4e3079197ecb2
tjlsgh <ubuntu@localhost.localdomain>
2024-03-20 03:41:15 +0800

    set lazyvim and leetcode

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
